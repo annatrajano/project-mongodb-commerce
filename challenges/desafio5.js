bc4995e2ddeb0233969b73e4985fa42e5cc7338d
annatrajano <annatrajano95@gmail.com>
2022-09-08 16:49:47 -0300

    refactor: :memo: req01 to req05 - using find() and sort()

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,2 +1,11 @@
-db.produtos.find({ $or: [{ curtidas: { $eq: 36 } }, { vendidos: { $gt: 85 } }] }, 
-    { _id: 0, nome: 1, vendidos: 1, curtidas: 1 });
+db.produtos.find({ 
+    $or: [
+        { curtidas: { $eq: 36 } }, 
+        { vendidos: { $eq: 85 } },
+    ] }, 
+    { 
+        _id: 0, 
+        nome: 1, 
+        curtidas: 1,
+        vendidos: 1, 
+    });
